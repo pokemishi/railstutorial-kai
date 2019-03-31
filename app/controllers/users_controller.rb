60d26bb41b12415a16d650fcfc849e33a94c9084
EC2 Default User <yasuhiro.mishima001@gmail.com>
2019-03-31 23:36:10 +0900

    ユーザーの更新処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
-    @user = User.new(user_params) 
+    @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -18,10 +19,36 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
-  private
-  
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    
+    # beforeアクション
+    
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
 end
